710af5f823f691083c814476f35c72cb3ded01ac
Tomas <tomasfreire@gmail.com>
2022-06-03 18:36:29 -0300

    fixed medium link error

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     description: `A blockchain/ethereum/solidity smart contracts security blog`,
     siteUrl: `https://www.tomasfreire.com`,
     social: {
-      medium: `tomasfrancisco`,
+      medium: `@tomasfrancisco`,
     },
   },
   plugins: [
